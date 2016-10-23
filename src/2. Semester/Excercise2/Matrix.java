2275d968bfea7eb1570c82c4547c1ade903601c8
Tom <duke64@gmx.de>
2016-10-23 12:59:05 +0200

    Finetuning

--- src/2. Semester/Excercise2/Matrix.java
+++ src/2. Semester/Excercise2/Matrix.java
@@ -1,12 +1,15 @@
 /**
- * blub
+ * Mit Matrix koennen double Matrizen erstellt werden.
+ * Einfache Anwendungen wie das Addieren, Multiplizieren mit einem Skalar oder einer anderen Matrix sind möglich
+ * Basis fuer weitere Methoden.
  *
  * @author Tom Dittrich s0555944@htw-berlin.de
- * @version 0.3
- * @date 19.10.16
+ * @version 1.0
+ * @date 23.10.16
  */
 public class Matrix {
 
+    // Matrix als Array
     private double[][] array;
     private int zeilen;
     private int spalten;
@@ -141,6 +144,7 @@ public class Matrix {
      * @return Ergebnis Matrix
      */
     public Matrix add(Matrix ma) {
+        // erzeugt Ergebnis Matrix mit den Dimensionen der ersten Matrix
         double[][] ergebnis = new double[getZeilen()][getSpalten()];
 
         if (getZeilen() == ma.getZeilen() && getSpalten() == ma.getSpalten()) {
@@ -150,6 +154,7 @@ public class Matrix {
                 }
             }
         } else {
+            // Exception, wenn Matrizen unterschiedliche Dimensionen haben
             throw new IllegalArgumentException("Matrizen Dimensionen nicht kompatibel");
         }
         return new Matrix(ergebnis);
@@ -162,6 +167,7 @@ public class Matrix {
      * @return Ergebnix Matrix
      */
     public Matrix multiplyScalar(double scalar) {
+        // erzeugt Ergebnis Matrix mit den Dimensionen der ersten Matrix
         double[][] ergebnis = new double[getZeilen()][getSpalten()];
 
         for (int i = 0; i < array.length; i++) {
@@ -172,5 +178,29 @@ public class Matrix {
         return new Matrix(ergebnis);
     }
 
+    /**
+     * Multipliziert zwei Matrizen
+     *
+     * @param ma zu multiplizierendes Matrix-Objekt
+     * @return Ergebnis Matrix
+     */
+    public Matrix multiply(Matrix ma) {
+        // erzeugt Ergebnis Matrix mit den Zeilen der 1. und den Spalten der 2.Matrix
+        double[][] ergebnis = new double[getZeilen()][ma.getSpalten()];
 
+        // Exception, wenn Matrizen inkompatible Dimensionen haben
+        if (getSpalten() != ma.getZeilen()) {
+            throw new IllegalArgumentException("Falsche Dimensionen, Matrizen können nicht multipliziert werden.");
+        } else {
+            for (int i = 0; i < ergebnis.length; i++) {
+                for (int j = 0; j < ergebnis[0].length; j++) {
+                    for (int k = 0; k < ma.array.length; k++) {
+                        ergebnis[i][j] += array[i][k] * ma.array[k][j];
+                    }
+                }
+            }
+        }
+
+        return new Matrix(ergebnis);
+    }
 }
--- src/2. Semester/Excercise2/MatrixTest.java
+++ src/2. Semester/Excercise2/MatrixTest.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
  * JUnit Testklasse fuer die Klasse Matrix
  *
  * @author Tom Dittrich s0555944@htw-berlin.de
- * @version 0.3
- * @date 19.10.16
+ * @version 1.0
+ * @date 23.10.16
  */
 public class MatrixTest {
 
@@ -35,6 +35,7 @@ public class MatrixTest {
 
     /**
      * Konstruktor Test, ungültiges Array (4. Zeile zu kurz), soll Exception werfen
+     * Fehlerfall
      */
     @Test(expected = IllegalArgumentException.class)
     public void KonstruktorMatrixFalschTest() {
@@ -46,11 +47,9 @@ public class MatrixTest {
         );
     }
 
-    // assertTrue(Arrays.deepEquals(ma1.getArray(), ma2.getArray()));
-    // assertArrayEquals(ma1.getArray(), ma2.getArray());
-
     /**
      * Test ob die Vergleichs Methode richtig arbeitet
+     * Normalfall
      */
     @Test
     public void equalsTest() {
@@ -58,64 +57,168 @@ public class MatrixTest {
         assertTrue(ma1.equals(ma2));
 
         // 1.Zeile 1.Spalte ist eine andere Zahl, False Prüfung
-        Matrix ma10 = new Matrix(new double[][]{new double[]{2, 2, 3}, new double[]{4, 5, 6}, new double[]{7, 8, 9}});
+        Matrix ma10 = new Matrix(new double[][]{
+                new double[]{2, 2, 3},
+                new double[]{4, 5, 6},
+                new double[]{7, 8, 9}});
         assertFalse(ma1.equals(ma10));
 
         // 3.Zeile 3.Spalte ist eine andere Zahl, False Prüfung
-        Matrix ma11 = new Matrix(new double[][]{new double[]{1, 2, 3}, new double[]{4, 5, 6}, new double[]{7, 8, -8}});
+        Matrix ma11 = new Matrix(new double[][]{
+                new double[]{1, 2, 3},
+                new double[]{4, 5, 6},
+                new double[]{7, 8, -8}});
         assertFalse(ma1.equals(ma11));
 
         // 3*3 mit 2*2 Vergleich, False Prüfung
-        Matrix ma12 = new Matrix(new double[][]{new double[]{1, 2}, new double[]{4, 5}, new double[]{7, 8}});
+        Matrix ma12 = new Matrix(new double[][]{
+                new double[]{1, 2},
+                new double[]{4, 5},
+                new double[]{7, 8}});
         assertFalse(ma1.equals(ma12));
 
         // 3*3 mit 4*4 Vergleich, False Prüfung
-        Matrix ma13 = new Matrix(new double[][]{new double[]{1, 2, 3, 4}, new double[]{4, 5, 6, 7}, new double[]{7, 8, 9, -10}});
+        Matrix ma13 = new Matrix(new double[][]{
+                new double[]{1, 2, 3, 4},
+                new double[]{4, 5, 6, 7},
+                new double[]{7, 8, 9, -10}});
         assertFalse(ma1.equals(ma13));
 
     }
 
     /**
      * Test ob die Additions Methode richtig arbeitet
+     * Normalfall
      */
     @Test
     public void addTest() {
         // zwei Default 3*3 Matrizen werden addiert, ma10 ist die erwartete Matrix, Equal Prüfung
-        Matrix ma10 = new Matrix(new double[][]{new double[]{2, 4, 6}, new double[]{8, 10, 12}, new double[]{14, 16, 18}});
+        Matrix ma10 = new Matrix(new double[][]{
+                new double[]{2, 4, 6},
+                new double[]{8, 10, 12},
+                new double[]{14, 16, 18}});
         assertArrayEquals(ma10.getArray(), ma1.add(ma2).getArray());
 
         // zwei Default 3*3 Matrizen werden addiert, ma11 ist die (falsche) erwartete Matrix, False Prüfung
-        Matrix ma11 = new Matrix(new double[][]{new double[]{1, 1, 1}, new double[]{4, 5, 6}, new double[]{7, 8, 9}});
+        Matrix ma11 = new Matrix(new double[][]{
+                new double[]{1, 1, 1},
+                new double[]{4, 5, 6},
+                new double[]{7, 8, 9}});
         assertFalse(Arrays.deepEquals(ma11.getArray(), ma1.add(ma2).getArray()));
     }
 
     /**
+     * Test ob die Additions Methode richtig arbeitet, mit Grenzwerten von double
+     * Grenzfall
+     */
+    @Test
+    public void addTestGrenzfallTest(){
+        // zwei Matrizen mit den maximal und minimal Werten werden gegeneinander addiert
+        // ma12 ist die erwartete Null-Matrix, Equal Prüfung
+        Matrix ma10 = new Matrix(new double[][]{
+                new double[]{Double.MIN_VALUE, Double.MAX_VALUE},
+                new double[]{-Double.MIN_VALUE, -Double.MAX_VALUE}});
+        Matrix ma11 = new Matrix(new double[][]{
+                new double[]{-Double.MIN_VALUE, -Double.MAX_VALUE},
+                new double[]{Double.MIN_VALUE, Double.MAX_VALUE}});
+        Matrix ma12 = new Matrix(new double[][]{
+                new double[]{0, 0},
+                new double[]{0, 0}});
+        assertArrayEquals(ma12.getArray(), ma10.add(ma11).getArray());
+    }
+    /**
      * Test ob die Additions Methode richtig arbeitet
      * nicht mögliche Addition von verschiedenen Dimensionen, soll Exception werfen
+     * Fehlerfall
      */
     @Test(expected = IllegalArgumentException.class)
     public void addFalschTest() {
         //3*3 Matrix wird mit 2*2 addiert, nicht möglich
-        Matrix ma10 = new Matrix(new double[][]{new double[]{1, 2}, new double[]{3, 4}, new double[]{5, 6}});
+        Matrix ma10 = new Matrix(new double[][]{
+                new double[]{1, 2},
+                new double[]{3, 4},
+                new double[]{5, 6}});
         ma1.add(ma10);
     }
 
     /**
      * Test ob die Multiplikation mit einem Skalar richtig arbeitet
+     * Normalfall
      */
     @Test
     public void multiplyScalarTest(){
         // Default Matrix wird mit 2 multipliziert, ma10 ist die erwartete Matrix, Equal Prüfung
-        Matrix ma10 = new Matrix(new double[][]{new double[]{2, 4, 6}, new double[]{8, 10, 12}, new double[]{14, 16, 18}});
+        Matrix ma10 = new Matrix(new double[][]{
+                new double[]{2, 4, 6},
+                new double[]{8, 10, 12},
+                new double[]{14, 16, 18}});
         assertArrayEquals(ma10.getArray(), ma1.multiplyScalar(2).getArray());
 
-        //// Default Matrix wird mit -1 multipliziert, ma11 ist die (falsche) erwartete Matrix, False Prüfung
-        Matrix ma11 = new Matrix(new double[][]{new double[]{-1, -2, -3}, new double[]{-4, -5, -6}, new double[]{-7, -8, 9}});
+        // Default Matrix wird mit -1 multipliziert, ma11 ist die (falsche) erwartete Matrix, False Prüfung
+        Matrix ma11 = new Matrix(new double[][]{
+                new double[]{-1, -2, -3},
+                new double[]{-4, -5, -6},
+                new double[]{-7, -8, 9}});
         assertFalse(Arrays.deepEquals(ma11.getArray(), ma1.multiplyScalar(-1).getArray()));
 
-        //// Default Matrix wird mit 10.5 multipliziert, ma12 ist die erwartete Matrix, True Prüfung
-        Matrix ma12 = new Matrix(new double[][]{new double[]{10.5, 21, 31.5}, new double[]{42, 52.5, 63}, new double[]{73.5, 84.0, 94.5}});
+        // Default Matrix wird mit 10.5 multipliziert, ma12 ist die erwartete Matrix, True Prüfung
+        Matrix ma12 = new Matrix(new double[][]{
+                new double[]{10.5, 21, 31.5},
+                new double[]{42, 52.5, 63},
+                new double[]{73.5, 84.0, 94.5}});
         assertTrue(Arrays.deepEquals(ma12.getArray(), ma1.multiplyScalar(10.5).getArray()));
+    }
+
+    /**
+     * Test ob die Multiplikations Methode mit zwei Matrizen arbeitet
+     * Normalfall
+     */
+    @Test
+    public void multiplyTest(){
+        // zwei Default 3*3 Matrizen werden multipliziert, ma10 ist die erwartete Matrix, Equal Prüfung
+        Matrix ma10 = new Matrix(new double[][]{
+                new double[]{30, 36, 42},
+                new double[]{66, 81, 96},
+                new double[]{102, 126, 150}});
+        assertArrayEquals(ma10.getArray(), ma1.multiply(ma2).getArray());
+
+        // zwei Default 3*3 Matrizen werden multipliziert, ma11 ist die (falsche) erwartete Matrix, False Prüfung
+        Matrix ma11 = new Matrix(new double[][]{
+                new double[]{30, 36, 42},
+                new double[]{66, 81, 96},
+                new double[]{-102, -126, -150}});
+        assertFalse(Arrays.deepEquals(ma11.getArray(), ma1.multiply(ma2).getArray()));
+
+        // 4x2 Matrix ma12 wird mit 2x3 Matrix ma13 multipliziert, ma14 ist die erwartete 4x3 Natruxm Equal Prüfung
+        Matrix ma12 = new Matrix(new double[][]{
+                new double[]{1.5, 2.5},
+                new double[]{3, 4},
+                new double[]{5, 6},
+                new double[]{7,8}});
+        Matrix ma13 = new Matrix(new double[][]{
+                new double[]{1.5, 2.5, 3},
+                new double[]{4, 5, 6}});
+        Matrix ma14 = new Matrix(new double[][]{
+                new double[]{12.25, 16.25, 19.5},
+                new double[]{20.5, 27.5, 33},
+                new double[]{31.5, 42.5, 51},
+                new double[]{42.5, 57.5, 69}});
+        assertArrayEquals(ma14.getArray(), ma12.multiply(ma13).getArray());
+    }
+
+    /**
+     * Test ob die Multiplikations Methode richtig arbeitet
+     * nicht mögliche Multiplikationen von verschiedenen Dimensionen, soll Exception werfen
+     * Fehlerfall
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void multiplyFalschTest(){
+        //Default 3*3 Matrix wird mit einer 2*3 multipliziert, nicht möglich
+        Matrix ma13 = new Matrix(new double[][]{
+                new double[]{1.5, 2.5, 3},
+                new double[]{4, 5, 6}});
+        ma1.multiply(ma13);
+
     }
 
 }
