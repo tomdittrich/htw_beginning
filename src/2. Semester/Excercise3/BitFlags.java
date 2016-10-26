bfb9f0c60304ca0320523567031296379d00ef40
Tom <duke64@gmx.de>
2016-10-26 21:58:22 +0200

    add 3. Excercise, tbc

---
+++ src/2. Semester/Excercise3/BitFlags.java
@@ -0,0 +1,90 @@
+/**
+ * Mit BitFlags können die einzelnen Bits einer Integer Zahl verändert werden.
+ * Basis für weitere Methoden.
+ *
+ * @author Tom Dittrich s0555944@htw-berlin.de
+ * @version 0.5
+ * @date 26.10.16
+ */
+public class BitFlags {
+
+    private int status;
+
+    /**
+     * Konstruktor
+     * Zu ubergebende Integer Zahl, die bearbeitet werden soll
+     *
+     * @param status zu übergebende Zahl
+     */
+    public BitFlags(int status) {
+        setStatus(status);
+    }
+
+    /**
+     * Getter Zahl
+     *
+     * @return Zahl
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * Setter Zahl
+     *
+     * @param status zu übergebende Zahl
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    /**
+     * Setzt das Bit bei Indexposition auf 1
+     *
+     * @param index Indexposition
+     */
+    public void switchOn(int index) {
+        int hilf = 1;
+
+        hilf = hilf << index;
+        this.status = this.status | hilf;
+    }
+
+    /**
+     * Setzt das Bit bei Indexposition auf 0
+     *
+     * @param index Indexposition
+     */
+    public void switchOff(int index) {
+        int hilf = 1;
+
+        hilf = ~(hilf << index);
+        this.status = this.status & hilf;
+    }
+
+    /**
+     * dreht das Bit bei Indexposition um
+     *
+     * @param index Indexposition
+     */
+    public void swap(int index) {
+        int hilf = 1;
+
+        hilf = hilf << index;
+        this.status = this.status ^ hilf;
+
+    }
+
+    /**
+     * gibt zurück, ob Bit bei Indexposition gesetzt ist
+     *
+     * @param index Indexposition
+     * @return gesetzt? True
+     */
+    public boolean isSet(int index) {
+        int hilf = 1;
+
+        hilf = hilf << index;
+        return ((this.status & hilf) == hilf)?true:false;
+    }
+}
---
+++ src/2. Semester/Excercise3/BitFlagsTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Tom on 26.10.2016.
+ */
+public class BitFlagsTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getStatus() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setStatus() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void switchOn() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void switchOff() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void swap() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void isSet() throws Exception {
+
+    }
+
+}
