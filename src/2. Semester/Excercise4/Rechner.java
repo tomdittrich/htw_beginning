0a709e345126644f0f2dcff1266c20a794d6534b
Tom <duke64@gmx.de>
2016-11-06 20:34:20 +0100

    Add: Documentation and Testclass

--- src/2. Semester/Excercise4/Rechner.java
+++ src/2. Semester/Excercise4/Rechner.java
@@ -1,17 +1,38 @@
 /**
- * Mit Rechner können einfache Rechenoperationen durchgeführt werden
+ * Mit Rechner können einfache Rechenoperationen durchgeführt werden.
+ * Übergabe durch args Argumente: Zahl1 [add|sub|mul|div] Zahl2
  *
  * @author Tom Dittrich s0555944@htw-berlin.de
- * @version 0.5
- * @date 02/11/16
+ * @version 0.7
+ * @date 06/11/16
  */
 public class Rechner {
 
     public static void main(String[] args) {
-        double a = Double.parseDouble(args[0]);
-        double b = Double.parseDouble(args[2]);
-        String op = args[1];
 
+        output(args);
+    }
+
+    /**
+     * gibt die Rechnung und das Ergebnis aus
+     *
+     * @param werte zu übergebende String-args Array
+     */
+    private static void output (String[] werte){
+        // prüft ob die Anzahl der Argumente übereinstimmt
+        if(werte.length!=3){
+            throw new IllegalArgumentException("Ungültige Menge an Argumenten.");
+        }
+
+        double a = Double.parseDouble(werte[0]);
+        double b = Double.parseDouble(werte[2]);
+
+        String op = werte[1];
+
+
+        // Prüft, welche Rechenoperation statt findet.
+        // Die Rechenop. sind mit Absicht ausgelagert: sollte das Programm ggf. weiter geschrieben
+        // werden, könnenten die Methoden noch nützlich sein
         if (op.equals("add")) {
             System.out.println(a + " " + op + " " + b + " ist " + add(a, b));
 
@@ -22,25 +43,57 @@ public class Rechner {
             System.out.println(a + " " + op + " " + b + " ist " + mul(a, b));
 
         } else if (op.equals("div")) {
-            System.out.println(a + " " + op + " " + b + " ist " + div(a, b));
+            if(b!=0){
+                System.out.println(a + " " + op + " " + b + " ist " + div(a, b));
+            } else {
+                System.out.println("Division durch 0 nicht möglich.");
+            }
 
         } else {
             throw new IllegalArgumentException("Keine gültige Operation angegeben.");
         }
     }
 
+    /**
+     * Addition von zwei Zahlen
+     *
+     * @param a Zahl1
+     * @param b Zahl2
+     * @return auf zwei stellen gerundetes Ergebnis
+     */
     private static double add(double a, double b) {
         return (Math.round( (a+b) * 100 ) / 100.0);
     }
 
+    /**
+     * Subtraktion von zwei Zahlen
+     *
+     * @param a Zahl1
+     * @param b Zahl2
+     * @return auf zwei stellen gerundetes Ergebnis
+     */
     private static double sub(double a, double b) {
         return (Math.round( (a-b) * 100 ) / 100.0);
     }
 
+    /**
+     * Multiplikation von zwei Zahlen
+     *
+     * @param a Zahl1
+     * @param b Zahl2
+     * @return auf zwei stellen gerundetes Ergebnis
+     */
     private static double mul(double a, double b) {
         return (Math.round( (a*b) * 100 ) / 100.0);
     }
 
+    /**
+     * Division von zwei Zahlen
+     *
+     * @param a Zahl1
+     * @param b Zahl2
+     * @return auf zwei stellen gerundetes Ergebnis
+     */
     private static double div(double a, double b) {
         return (Math.round( (a/b) * 100 ) / 100.0);
     }
--- src/2. Semester/Excercise4/RechnerTest.java
+++ src/2. Semester/Excercise4/RechnerTest.java
@@ -2,22 +2,54 @@
  * Reine Testklasse für die Klasse Rechner
  *
  * @author Tom Dittrich s0555944@htw-berlin.de
- * @version 0.5
- * @date 02/11/16
+ * @version 0.7
+ * @date 06/11/16
  */
 public class RechnerTest {
 
     public static void main(String[] args) {
-        String[] test1 = {"5", "add", "5"};
-        Rechner.main(test1);
+        String[][] test = new String[][]{
+                //Addition
+                new String[]{"2", "add", "2"},
+                new String[]{"-5", "add", "2"},
+                new String[]{"5", "add", "-2"},
+                new String[]{"-5", "add", "-5"},
+                new String[]{"2.7", "add", "2.7"},
+                new String[]{"2.74", "add", "2.73"},
+                new String[]{"5", "add", "0"},
 
-        String[] test2 = {"5", "sub", "16.4"};
-        Rechner.main(test2);
+                //Subtraktion
+                new String[]{"3", "sub", "2"},
+                new String[]{"-5", "sub", "2"},
+                new String[]{"5", "sub", "-2"},
+                new String[]{"-5", "sub", "-6"},
+                new String[]{"2.7", "sub", "2.6"},
+                new String[]{"2.89", "sub", "2.73"},
+                new String[]{"5", "sub", "0"},
 
-        String[] test3 = {"5", "mul", "-2.3"};
-        Rechner.main(test3);
+                //Multiplikation
+                new String[]{"3", "mul", "2"},
+                new String[]{"-5", "mul", "2"},
+                new String[]{"5", "mul", "-2"},
+                new String[]{"-5", "mul", "-6"},
+                new String[]{"2.7", "mul", "2.6"},
+                new String[]{"2.34", "mul", "2.73"},
+                new String[]{"5", "mul", "0"},
 
-        String[] test4 = {"8.8", "div", "4.25"};
-        Rechner.main(test4);
+                //Division
+                new String[]{"6", "div", "2"},
+                new String[]{"-6", "div", "2"},
+                new String[]{"6", "div", "-2"},
+                new String[]{"-12", "div", "-4"},
+                new String[]{"12.55", "div", "6.21"},
+                new String[]{"0", "div", "6"},
+                new String[]{"5", "div", "0"},
+        };
+
+        for (int i = 0; i < test.length; i++) {
+            Rechner.main(test[i]);
+        }
+
+
     }
 }
